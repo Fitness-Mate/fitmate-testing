16b540f5b7560e28e81c4e32dff07b91f005e2ce
Whoknow77 <Whoknow77@users.noreply.github.com>
2023-08-11 06:43:08 +0000

    Update from Frontend repo by Whoknow77

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,14 +1,8 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
-const dotenv = require("dotenv");
-
-// 환경변수 로드
-dotenv.config();
-
-const API_TARGET = process.env.REACT_APP_DOMAIN;
 module.exports = (app) => {
   app.use(
     createProxyMiddleware("/api", {
-      target: API_TARGET,
+      target: "http://43.200.20.25:8080",
       changeOrigin: true,
       pathRewrite: { "^/api": "" },
     })
